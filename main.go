5fe684f6b62f2107d2ab7e935261ae04fe7b5656
Chiagoziem Sunday <chiagoziem.ms@gmail.com>
2026-04-27 23:00:02 +0100

    feat: implement logic for basic file search

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-  fmt.Println("Needle is a grep-like search tool built in Go!")
+	// accept positional args
+	if len(os.Args) < 3 {
+		fmt.Fprintln(os.Stderr, "Need two parameters: pattern and file path")
+		os.Exit(1)
+	}
+
+	// extract args pattern and file path
+	pattern, path := os.Args[1], os.Args[2]
+
+	// open file from file path and handle error
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	lineNumber := 0
+
+	// scan the file, and return the lines that match the pattern
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lineNumber++
+		if strings.Contains(scanner.Text(), pattern) {
+			fmt.Println(lineNumber, scanner.Text())
+		}
+	}
+  // return err if an error occurs
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "scanning file", err)
+		os.Exit(1)
+	}
 }
